60d35e19fc294608992447fe0bf57984eb460c9d
Mahmoud Shalash <59184028+Shalash96@users.noreply.github.com>
2022-11-08 00:49:16 +0000

    Update SolPred_websit.py

--- website/SolPred_websit.py
+++ website/SolPred_websit.py
@@ -197,7 +197,7 @@ with left:
 
       xyzview.zoomTo()
       # xyzview.png()
-      showmol(xyzview,height=500,width=500)
+      showmol(xyzview,height=500,width=350)
     try:
       mol_viewer(smiles_input[mol_number])
     except:
